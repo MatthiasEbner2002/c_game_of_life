ce09087e82b53aec0a85df39dbe60797e4249968
Matthias Ebner <csaz9531@student.uibk.ac.at>
2024-06-19 00:05:04 +0200

    add function pointer to the structs

--- main.c
+++ main.c
@@ -48,18 +48,22 @@ typedef struct {
 /*
  * @struct History
  * @brief The history of the game.
- * @param calc_time_history: List of the last histrory_size calc times.
+ * @param calc_time_history: List of the last history_size calc times.
  * @param calc_time_history_all: List of all calc_times. Size increases over time.
  * @param history_max_size: Save the max history size for calc_time_history_all.
- * @param histrory_size: The calc_time_history && the size increase for history_max_size if full.
+ * @param history_size: The calc_time_history && the size increase for history_max_size if full.
  * @param info_box_height: The height of the graph will be the info_box_height - 2.
+ * @param free_history: Pointer to the free function.
 **/
-typedef struct {
-    double *calc_time_history;  /* @brief List of the last histrory_size calc times. */
+typedef struct History {
+    double *calc_time_history;  /* @brief List of the last history_size calc times. */
     double *calc_time_history_all;  /* @brief List of all calc_times. Size increases over time */
     int history_max_size;  /* @brief Save the max history size for calc_time_history_all. */
-    int histrory_size;  /* @brief The calc_time_history && the size increase for history_max_size if full. */
+    int history_size;  /* @brief The calc_time_history && the size increase for history_max_size if full. */
     // the height of the graph will be the info_box_height - 2
+
+    // Functions:
+    void (*free_history)(struct History*); /* @brief Pointer to the free function. */
 } History;
 
 /*
@@ -73,10 +77,10 @@ typedef struct {
 * @param width: The width of the game window.
 * @param height: The height of the game window.
 * @param last_calc_time: The last calculation time.
-* @param count_cicles: The count of the cicles.
+* @param count_circles: The count of the cicles.
 * @param avg_calc_time: The average calculation time.
 **/
-typedef struct {
+typedef struct GameOfLife{
     WINDOW *game_window;
     WINDOW *info_box;
     Cell **cells;
@@ -85,10 +89,24 @@ typedef struct {
     int width;
     int height;
     double last_calc_time;
-    int count_cicles;
+    int count_circles;
     double avg_calc_time;
+
+    // Functions:
+    void (*update_game_x_y)(struct GameOfLife*);  /* @brief Updates the width and height of the game window. */
+    void (*free_game)(struct GameOfLife*);  /* @brief Frees the game. */
+    void (*update_cells)(struct GameOfLife*);  /* @brief Updates the cells of the game. */
+    void (*handle_resize)(struct GameOfLife*);  /* @brief Handles the resize of the game window. */
+    void (*draw_game_field)(struct GameOfLife*);  /* @brief Draws the game field. */
+    void (*draw_info_box)(struct GameOfLife*);  /* @brief Draws the info box. */
+    void (*handle_key_input)(struct GameOfLife*, bool*);  /* @brief Handles the key input. */
+    void (*update_history)(struct GameOfLife*);  /* @brief Updates the history. */
 } GameOfLife;
 
+/*
+ * Updates the width and height of the game window.
+ * @param game: the game to update.
+**/
 void update_game_x_y(GameOfLife *game) {
     if (game == NULL) return;
     getmaxyx(stdscr, game->height, game->width);  // Update the height and width of the game window
@@ -102,49 +120,48 @@ void update_game_x_y(GameOfLife *game) {
         game->width /= 2;
 }
 
+/*
+ * Frees the history.
+ * @param history: the history to free.
+**/
+void free_history(History *history){
+    if (history == NULL) return;
+    if (history->calc_time_history != NULL) free(history->calc_time_history);
+    if (history->calc_time_history_all != NULL) free(history->calc_time_history_all);
+    free(history);
+}
+
+/*
+ * Creates a new history.
+ * @param size: the size of the history.
+ * @return the new history.
+**/
 History* create_history(int size) {
     if (size <= 10){
         log_error("History size must be greater than 10");
         return NULL;
     }
     History *history = calloc(1, sizeof(History));
-    history->histrory_size = size;
+    history->history_size = size;
     history->history_max_size = size;
     history->calc_time_history = calloc(size, sizeof(double));
     history->calc_time_history_all = calloc(size, sizeof(double));
+    history->free_history = free_history;
     return history;
 }
 
 /*
- * Creates a new game of live. The cells will be initialized with random values.
- * The setttings can be NULL, then default settings will be used (created with calloc, so all false).
- * Height and width will be set to the size of the terminal and ajusted to the settings.
- * @param settings: the settings of the game, if NULL, default settings will be used.
- * @return the new game of life.
+ * Creates the settings for the game.
+ * The settings can be set with the following options:
+ * - [-2]: Display two cells per block.
+ * - [-nc]: No colors will be used.
+ * - [-nh]: Do not show history.
+ * - [-ni]: Do not show info at start.
+ * - [-h]: Show the help.
+ * @param argc: the number of arguments.
+ * @param argv: the arguments.
+ * @return the settings for the game.
 **/
-GameOfLife* create_game(Settings *settings) {
-    GameOfLife *game = calloc(1, sizeof(GameOfLife));
-    if (settings != NULL) game->settings = settings;
-    else game->settings = calloc(1, sizeof(Settings));
-
-    game->game_window = newwin(0, 0, 0, 0);
-    game->info_box = newwin(game->settings->info_box_height, 0, 0, 0);
-
-    update_game_x_y(game);
-    
-    game->cells = malloc(sizeof(Cell *) * game->height);
-    for (int i = 0; i < game->height; i++) {
-        game->cells[i] = malloc(game->width *  sizeof(Cell));
-        for (int j = 0; j < game->width; j++){
-            game->cells[i][j].alive = rand() % 2 == 0;
-            game->cells[i][j].alive_for_iterations = 0;
-        }
-    }
-    game->history = create_history(100);
-    return game;
-}
-
-
 Settings* create_settings(int argc, char *argv[]) {
     Settings *settings = calloc(1, sizeof(Settings));
     // Default settings
@@ -176,25 +193,27 @@ Settings* create_settings(int argc, char *argv[]) {
     return settings;
 }
 
-void free_history(History *history){
-    if (history == NULL) return;
-    if (history->calc_time_history != NULL) free(history->calc_time_history);
-    if (history->calc_time_history_all != NULL) free(history->calc_time_history_all);
-    free(history);
-}
-
+/*
+ * Frees the game.
+ * @param game: the game to free.
+**/
 void free_game(GameOfLife *game){
     if (game == NULL) return;
     if (game->game_window != NULL) delwin(game->game_window);
     if (game->info_box != NULL) delwin(game->info_box);
     if (game->settings != NULL) free(game->settings);
-    free_history(game->history);
+    game->history->free_history(game->history);
     for (int i = 0; i < game-> height; i++) 
         free(game->cells[i]);
     free(game->cells);
     free(game);
 }
 
+/*
+ * Updates the cells of the game.
+ * The cells will be updated according to the rules of the game of life.
+ * @param game: the game to update the cells for.
+**/
 void update_cells(GameOfLife *game) {
     if (game == NULL) return;
 
@@ -245,6 +264,11 @@ void update_cells(GameOfLife *game) {
     free(old_cells);
 }
 
+/*
+ * Handles the resize of the game window.
+ * The cells will be resized and the new cells will be initialized with random values.
+ * @param game: the game to handle the resize for.
+**/
 void handle_resize(GameOfLife *game){
     if (game == NULL) return;
     int old_height = game->height;
@@ -289,7 +313,11 @@ void handle_resize(GameOfLife *game){
             game->cells[i] = realloc(game->cells[i], sizeof(Cell) * game->width);
     }
 }
-
+/*
+ * Returns the color of the cell. The color depends on the number of iterations the cell is alive.
+ * @param cell: the cell to get the color for.
+ * @return the color of the cell.
+**/
 int get_cell_color(Cell *cell) {
     if (cell == NULL) {
         log_error("Cell is NULL, return color 1.");
@@ -339,6 +367,13 @@ void draw_game_field(GameOfLife *game) {
     }
 }
 
+/*
+ * Calculates the average of the given array.
+ * If one of the values is 0, the function will return 0.
+ * @param arr: the array to calculate the average for.
+ * @param size: the size of the array.
+ * @return the average of the array.
+**/
 double calculate_average(double arr[], int size) {
   double sum = 0;
   for (int i = 0; i < size; i++) {
@@ -348,6 +383,14 @@ double calculate_average(double arr[], int size) {
   return sum / size;
 }
 
+/*
+ * Downsamples the data and aggregates it.
+ * The data will be divided into num_buckets buckets and the average of each bucket will be calculated.
+ * @param data: the data to downsample and aggregate.
+ * @param size: the size of the data.
+ * @param num_buckets: the number of buckets to downsample to.
+ * @return the aggregated data.
+**/
 double* downsample_and_aggregate(double data[], int size, int num_buckets) {
     int bucket_size = size / num_buckets;
     double * aggregated_data = calloc(num_buckets, sizeof(double));
@@ -359,6 +402,10 @@ double* downsample_and_aggregate(double data[], int size, int num_buckets) {
     return aggregated_data;
 }
 
+/*
+ * Draws the info box at the bottom of the screen.
+ * @param game: the game to draw the info box for.
+**/
 void draw_info_box(GameOfLife *game) {
     if (game == NULL) return;
     box(game->info_box, 0, 0); // Draw a box around the hole info_window
@@ -367,7 +414,7 @@ void draw_info_box(GameOfLife *game) {
     mvwprintw(game->info_box, 2, 1, "Grid: %dx%d (%d)", game->width, game->height, game->width * game->height);
     mvwprintw(game->info_box, 3, 1, "Last calculation time   : %.6f sec", game->last_calc_time);
     mvwprintw(game->info_box, 4, 1, "Average calculation time: %.6f sec", game->avg_calc_time);
-    mvwprintw(game->info_box, 5, 1, "Cicles: %d", game->count_cicles);
+    mvwprintw(game->info_box, 5, 1, "Cicles: %d", game->count_circles);
     mvwprintw(game->info_box, game->settings->info_box_height - 3, 1, "[q]uit [r]eset [p]ause");
     mvwprintw(game->info_box, game->settings->info_box_height - 2, 1, "[c]olors [h]istory [2]mode");
 
@@ -377,11 +424,11 @@ void draw_info_box(GameOfLife *game) {
     
     double *total_history = downsample_and_aggregate(game->history->calc_time_history_all,
                                                      game->history->history_max_size,
-                                                     game->history->histrory_size);
+                                                     game->history->history_size);
 
     double *graph_data[2] = {game->history->calc_time_history, total_history}; // have different index calc in the loop
     int graph_height = game->settings->info_box_height - 2;
-    int graph_width = game->history->histrory_size;
+    int graph_width = game->history->history_size;
     int j_offset = 40; // The starting offset to the lest of the graphs
     int min_graph_width = 8;  // Min width to show a graph
     for (int k = 0; k < 2; k++){
@@ -415,7 +462,7 @@ void draw_info_box(GameOfLife *game) {
 
                 // Calculate the index of the calc time in the history array
                 int index = j;
-                if (k == 0) index = (game->count_cicles - graph_width + j) % graph_width;
+                if (k == 0) index = (game->count_circles - graph_width + j) % graph_width;
 
                 // Calculate the scaled calc time
                 double scaled_calc_time = (data[index] - min_calc_time) / calc_time_scale;
@@ -431,6 +478,12 @@ void draw_info_box(GameOfLife *game) {
     free(total_history);
 }
 
+/*
+ * Handles the key input. The following keys are supported:
+ * - [q]uit, [p]ause, [i]nfo, [c]olors, [h]istory, [2]mode, [r]eset
+ * @param game: the game to handle the input for.
+ * @param running: the running flag. if set to false, the game will stop.
+**/
 void handle_key_input(GameOfLife *game, bool *running) {
     int ch = getch();
     switch (ch) {
@@ -460,30 +513,74 @@ void handle_key_input(GameOfLife *game, bool *running) {
                     game->cells[i][j].alive_for_iterations = 0;
                 }
             }
-            game->count_cicles = 0;
+            game->count_circles = 0;
             game->last_calc_time = 0;
             game->avg_calc_time = 0;
             // Reset the history
-            int old_history_size = game->history->histrory_size;
-            free_history(game->history);
+            int old_history_size = game->history->history_size;
+            game->history->free_history(game->history);
             game->history = create_history(old_history_size);
             break;
     }
 }
 
+/*
+ * Updates the history. The history will be updated with the last calculation time.
+ * If the history is full, the size will be increased.
+ * @param game: the game to update the history for.
+**/
 void update_history(GameOfLife *game){
     History *h = game->history;
     if (h == NULL) return;
 
-    h->calc_time_history[game->count_cicles % h->histrory_size] = game->last_calc_time;
-    h->calc_time_history_all[game->count_cicles] = game->last_calc_time;
-    if (game->count_cicles - h->history_max_size + 1 == 0) {
-        h->history_max_size += h->histrory_size;
+    h->calc_time_history[game->count_circles % h->history_size] = game->last_calc_time;
+    h->calc_time_history_all[game->count_circles] = game->last_calc_time;
+    if (game->count_circles - h->history_max_size + 1 == 0) {
+        h->history_max_size += h->history_size;
         double *new_array = calloc(h->history_max_size, sizeof(double));
-        memcpy(new_array, h->calc_time_history_all, (h->history_max_size - h->histrory_size) * sizeof(double));
+        memcpy(new_array, h->calc_time_history_all, (h->history_max_size - h->history_size) * sizeof(double));
         free(h->calc_time_history_all);
         h->calc_time_history_all = new_array;
     }
+}
+
+/*
+ * Creates a new game of live. The cells will be initialized with random values.
+ * The setttings can be NULL, then default settings will be used (created with calloc, so all false).
+ * Height and width will be set to the size of the terminal and ajusted to the settings.
+ * @param settings: the settings of the game, if NULL, default settings will be used.
+ * @return the new game of life.
+**/
+GameOfLife* create_game(Settings *settings) {
+    GameOfLife *game = calloc(1, sizeof(GameOfLife));
+    if (settings != NULL) game->settings = settings;
+    else game->settings = calloc(1, sizeof(Settings));
+
+    game->game_window = newwin(0, 0, 0, 0);
+    game->info_box = newwin(game->settings->info_box_height, 0, 0, 0);
+
+    update_game_x_y(game);
+
+    game->cells = malloc(sizeof(Cell *) * game->height);
+    for (int i = 0; i < game->height; i++) {
+        game->cells[i] = malloc(game->width *  sizeof(Cell));
+        for (int j = 0; j < game->width; j++){
+            game->cells[i][j].alive = rand() % 2 == 0;
+            game->cells[i][j].alive_for_iterations = 0;
+        }
+    }
+    game->history = create_history(100);
+
+    // Add functions to the game
+    game->update_game_x_y = update_game_x_y;
+    game->free_game = free_game;
+    game->update_cells = update_cells;
+    game->handle_resize = handle_resize;
+    game->draw_game_field = draw_game_field;
+    game->draw_info_box = draw_info_box;
+    game->handle_key_input = handle_key_input;
+    game->update_history = update_history;
+    return game;
 }
 
 int main(int argc, char *argv[]) {
@@ -519,32 +616,32 @@ int main(int argc, char *argv[]) {
     bool running = true;
     while (running) {
         start_time = omp_get_wtime();
-        handle_resize(game);
+        game->handle_resize(game);
         if (!game->settings->pause)
-            update_cells(game);
+            game->update_cells(game);
 
         wclear(game->game_window);
-        draw_game_field(game);
+        game->draw_game_field(game);
         wrefresh(game->game_window);
     
         if (game->settings->show_info) {
             wclear(game->info_box);
-            draw_info_box(game);
+            game->draw_info_box(game);
             wrefresh(game->info_box);
         }
 
         game->last_calc_time = omp_get_wtime() - start_time;
         if (!game->settings->pause) {
-            update_history(game);
-            game->count_cicles++;
-            game->avg_calc_time = (game->avg_calc_time * (game->count_cicles - 1) + game->last_calc_time) / game->count_cicles;
+            game->update_history(game);
+            game->count_circles++;
+            game->avg_calc_time = (game->avg_calc_time * (game->count_circles - 1) + game->last_calc_time) / game->count_circles;
         }
 
-        handle_key_input(game, &running);
+        game->handle_key_input(game, &running);
         
         usleep(DELAY);
     }
-    free_game(game);
+    game->free_game(game);
     delwin(win);
     endwin();
     return EXIT_SUCCESS;
